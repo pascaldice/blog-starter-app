e33bbf743da6b43013cc939c8970f55e80896679
Pascal <yongji.shin@daewooenc.com>
2024-11-16 16:52:52 +0900

    api: enable revalidation for test route and disable dynamic rendering

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
-// export const revalidate = 60;
-export const dynamic = "force-dynamic";
+export const revalidate = 60;
+// export const dynamic = "force-dynamic";
 
 export async function GET() {
    return NextResponse.json({ message: "Hello from Server", date: new Date() });
