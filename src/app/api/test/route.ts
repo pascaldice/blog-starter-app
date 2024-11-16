536613366bfe745e3101b6a00c904ef5acc5bfa2
Pascal <yongji.shin@daewooenc.com>
2024-11-16 23:20:27 +0900

    api: update GET route to fetch and return data from external API

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -4,5 +4,8 @@ export const revalidate = 60;
 // export const dynamic = "force-dynamic";
 
 export async function GET() {
-   return NextResponse.json({ message: "Hello from Server", date: new Date() });
+   // return NextResponse.json({ message: "Hello from Server", date: new Date() });
+   const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+   const data = await res.json();
+   return NextResponse.json(data);
 }
